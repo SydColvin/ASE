62f0295fc104f9c5df1ac5fecf67f5d400941a03
st148 <57718210+st148@users.noreply.github.com>
2020-08-02 16:18:51 +0100

    Update GuiFrame.java

--- GuiFrame.java
+++ GuiFrame.java
@@ -9,20 +9,14 @@ import model.Airport;
 //GuiFrame for displaying desk information.
 
 public class GuiFrame extends JFrame {
-	/* Instant Variable from FlightList, PassengerList and Airport
-	 * 
-	 */
-    //private FlightList flightList;
-    //private PassengerList passengerList;
-    //private Airport airportDesk;
-    //private BaggageList baggageList;
-    
+	private JButton updateButton = new JButton("Update");
 	
 	//Create Frame & panel
-		public GuiFrame(Airport model){
+	public GuiFrame(Airport model){
 		
 		//Set Close by default close button of window
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
 		
 		//Gui Title
 		setTitle("Airport Check-in Status" ); 
@@ -33,9 +27,23 @@ public class GuiFrame extends JFrame {
 		add(BorderLayout.CENTER, new DeskDisplay(model));
 		//Set Flight Display Panel
 		add(BorderLayout.SOUTH, new FlightDisplay(model));
+		// Set Update 
+		add(BorderLayout.EAST, createUpdatePanel());
 
+		setVisible(true);
+
+	}
 	
-
+	public JPanel createUpdatePanel() {
+		JPanel updatePanel = new JPanel(new BorderLayout());
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(updateButton);
+		updatePanel.add(BorderLayout.NORTH,buttonPanel);	
+		return updatePanel;
+	}
+	
+	public void addSetListener(ActionListener al) {
+		updateButton.addActionListener(al);
 	}
 
 }
